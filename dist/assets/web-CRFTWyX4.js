6650a63e153cdac884c4441dde23fa84d1ccfb0f
Valentin Lafranca <valentin.lafranca@gmail.com>
2024-10-26 10:08:29 -0400

    Init repro

---
+++ dist/assets/web-CRFTWyX4.js
@@ -0,0 +1,12 @@
+import { W as WebPlugin } from "./index-Ci4a53u4.js";
+class SplashScreenWeb extends WebPlugin {
+  async show(_options) {
+    return void 0;
+  }
+  async hide(_options) {
+    return void 0;
+  }
+}
+export {
+  SplashScreenWeb
+};
